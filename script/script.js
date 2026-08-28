7afc26400a8f829dfc3148b435422b48e3301835
Gabriel Luna <glunamaia@gmail.com>
2026-08-28 13:22:10 -0300

    feat: adição de alert no envio do formulário

--- script/script.js
+++ script/script.js
@@ -3,6 +3,10 @@ const mobileMenu = document.querySelector(".mobile-menu");
 const navMobileMenu = document.querySelector(".nav-mobile-menu");
 const navLinks = document.querySelectorAll(".nav-hero-menu a");
 const sections = document.querySelectorAll("section[id]");
+const form = document.querySelector("#formContact")
+const input_nome = document.querySelector("#input-nome")
+const input_email = document.querySelector("#input-email")
+const input_mensagem = document.querySelector("#input-mensagem")
 
 window.addEventListener("scroll", () => {
     let activeSection = "";
@@ -23,6 +27,14 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    window.alert("Dados enviados com sucesso!")
+    input_email.value = ""
+    input_nome.value = ""
+    input_mensagem.value = ""
+})
 
 menuButton.addEventListener("click", () => {
     mobileMenu.classList.toggle("active")
